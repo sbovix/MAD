af35f79717fd09211967a5899321e40945d36bbe
AntonellaCupi <38326820+AntonellaCupi@users.noreply.github.com>
2018-04-18 18:38:26 +0200

    Riprova

---
+++ app/src/main/java/com/example/silviabova/mylogin/Book.java
@@ -0,0 +1,112 @@
+package com.example.silviabova.mylogin;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by cupia on 18/04/2018.
+ */
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private int edition_year;
+    private String book_condition;
+    private String extra;
+    private int libri=1;
+    private String imagestring;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getEdition_year() {
+        return edition_year;
+    }
+
+    public void setEdition_year(int edition_year) {
+        this.edition_year = edition_year;
+    }
+
+    public String getBook_condition() {
+        return book_condition;
+    }
+
+    public void setBook_condition(String book_condition) {
+        this.book_condition = book_condition;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+
+    public int getLibri() {
+        return libri;
+    }
+
+    public void setLibri(int libri) {
+        this.libri = libri;
+    }
+
+    public String getImagestring() {
+        return imagestring;
+    }
+
+    public void setImagestring(String imagestring) {
+        this.imagestring = imagestring;
+    }
+
+    public void saveBookInformation(FirebaseDatabase fb){
+
+        DatabaseReference myReftitle = fb.getReference("/Books/"+isbn+"/title");
+        DatabaseReference myRefauthor = fb.getReference("/Books/"+isbn+"/author");
+        DatabaseReference myRefpublisher = fb.getReference("/Books/"+isbn+"/publisher");
+        DatabaseReference myRefeditionyear= fb.getReference("/Books/"+isbn+"/edition_year");
+        DatabaseReference myRefbookcondition = fb.getReference("/Books/"+isbn+"/book_condition");
+        DatabaseReference myRefextra = fb.getReference("/Books/"+isbn+"/extra");
+        DatabaseReference myRefimage = fb.getReference("/Books/"+isbn+"/image");
+
+        myReftitle.setValue(title);
+        myRefauthor.setValue(author);
+        myRefpublisher.setValue(publisher);
+        myRefeditionyear.setValue(edition_year);
+        myRefbookcondition.setValue(book_condition);
+        myRefextra.setValue(extra);
+        myRefimage.setValue(imagestring);
+
+    }
+}
