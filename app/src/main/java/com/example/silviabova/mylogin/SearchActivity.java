51dc5ec67806bd417b7ceaba758b540d222e5f0c
MAD-Group25 <38313902+MAD-Group25@users.noreply.github.com>
2018-04-22 23:00:49 +0200

    Search layout

--- app/src/main/java/com/example/silviabova/mylogin/SearchActivity.java
+++ app/src/main/java/com/example/silviabova/mylogin/SearchActivity.java
@@ -3,19 +3,23 @@ package com.example.silviabova.mylogin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     public ListView lst;
+    public ArrayAdapter adapter;
 
     String[] LIST = {
-        "item", "item", "item1", "item1", "item1", "item1", "item1", "item1", "item1", "item1", "item1", "item1", "item1"
+        "item", "miao", "b", "item1", "h", "item1", "mamma", "item1", "bau", "item1", "item1", "item1", "item1"
     };
 
     @Override
@@ -23,7 +27,7 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         lst = (ListView)findViewById(R.id.ListView);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,LIST);
+         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,LIST);
         lst.setAdapter(adapter);
 
 
@@ -42,6 +46,29 @@ public class SearchActivity extends AppCompatActivity {
         Intent myIntent = new Intent(getApplicationContext(), HomeActivity.class);
         startActivityForResult(myIntent, 0);
         return true;
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search,menu);
+        MenuItem item = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
 
     }
 }
--- app/src/main/java/com/example/silviabova/mylogin/UserInfo.java
+++ app/src/main/java/com/example/silviabova/mylogin/UserInfo.java
@@ -18,4 +18,35 @@ public class UserInfo {
         this.URLimage = URLimage;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getURLimage() {
+        return URLimage;
+    }
+
+    public void setURLimage(String URLimage) {
+        this.URLimage = URLimage;
+    }
 }
